9231ba0f14f0fa81b0a32256d79008f6f76edb31
TMNRP <tony.venom007@gmail.com>
2021-06-10 03:17:40 +0530

    schedule slots

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import "./App.scss";
-import { Button, Descriptions, Dropdown, Menu, Table } from "antd";
+import { Button, Descriptions, Dropdown, Menu, Select, Table } from "antd";
 import { UserOutlined, DownOutlined } from "@ant-design/icons";
 import { useState } from "react";
+import { Option } from "antd/lib/mentions";
 
 //
 export const App = () => {
   return (
     <div className="App">
       <Header />
-      <div
-        style={{
-          padding: 10,
-          display: "grid",
-          rowGap: 20,
-          margin: 20,
-          boxShadow:
-            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-        }}
-      >
-        <EmployeeInfo />
-        <DependentList />
+      <div>
+        <div
+          style={{
+            padding: 10,
+            display: "grid",
+            rowGap: 20,
+            margin: 20,
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          }}
+        >
+          <EmployeeInfo />
+          <DependentList />
+        </div>
+
+        <div
+          style={{
+            padding: 10,
+            display: "grid",
+            rowGap: 20,
+            margin: 20,
+            boxShadow:
+              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          }}
+        >
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <ScheduleSlot />
+          </div>
+          <ScheduleDates />
+        </div>
       </div>
       <div>Footer</div>
     </div>
@@ -101,6 +120,7 @@ const DependentList = () => {
   return (
     <Table
       size="small"
+      pagination={false}
       columns={DependentListColumns}
       dataSource={DependentListData}
       rowSelection={{
@@ -149,12 +169,16 @@ const DependentListColumns = [
   {
     title: "Actions",
     key: "actions",
-    render: () => (
-      <div style={{ display: "grid", gridAutoFlow: "column", columnGap: 10 }}>
-        <Button type="primary">Reschedule</Button>
-        <Button>Cancel</Button>
-      </div>
-    ),
+    render: (t, r) => {
+      return (
+        <div style={{ display: "grid", gridAutoFlow: "column", columnGap: 10 }}>
+          <Button type="primary" disabled={!r?.["bookedDate"]}>
+            Reschedule
+          </Button>
+          <Button disabled={!r?.["bookedDate"]}>Cancel</Button>
+        </div>
+      );
+    },
   },
 ];
 const DependentListData = [
@@ -167,4 +191,96 @@ const DependentListData = [
     slot: "9:30-10:30",
     location: "Pune",
   },
+  {
+    name: "Anchit Father",
+    age: 45,
+    dose1: "Yes",
+    dose2: "No",
+    bookedDate: "2021-06-09",
+    slot: "9:30-10:30",
+    location: "Pune",
+  },
+  {
+    name: "Anchit Mother",
+    age: 40,
+    dose1: "Yes",
+    dose2: "No",
+    bookedDate: "",
+    slot: "",
+    location: "",
+  },
 ];
+
+//
+const ScheduleSlot = () => (
+  <div>
+    <div style={{ textAlign: "center" }}>
+      <h2>Schedule slot</h2>
+    </div>
+    <Select
+      showSearch
+      placeholder="City"
+      optionFilterProp="children"
+      style={{ width: 200 }}
+      filterOption={(input, option) =>
+        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      }
+    >
+      <Option value="Pune">Pune</Option>
+      <Option value="Banglore">Banglore</Option>
+    </Select>
+  </div>
+);
+
+//
+const ScheduleDates = () => (
+  <div
+    style={{
+      display: "grid",
+      gridAutoFlow: "column",
+      margin: "0px 50px",
+      gridTemplateColumns: "repeat(auto-fit, 75px)",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <ScheduleDateBlock label="Jun 09" value="117" />
+    <ScheduleDateBlock label="Jun 10" value="120" />
+    <ScheduleDateBlock label="Jun 11" value="12" />
+    <ScheduleDateBlock label="Jun 12" value="60" />
+  </div>
+);
+
+const ScheduleDateBlock = ({ label, value }) => (
+  <div style={{ width: 100 }}>
+    <div
+      style={{
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {label}
+    </div>
+    <div
+      style={{
+        color: "white",
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          padding: "5px 10px",
+          backgroundColor: "green",
+          borderRadius: 5,
+        }}
+      >
+        {value}
+      </div>
+    </div>
+  </div>
+);
